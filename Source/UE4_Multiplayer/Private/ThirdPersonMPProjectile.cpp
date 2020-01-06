5e478f1cfd13bba11ded0c2fffaa214ea9897974
vsensu.lr <vsensu.lr@alibaba-inc.com>
2020-01-06 18:03:22 +0800

    Creating a Projectile with Replication

---
+++ Source/UE4_Multiplayer/Private/ThirdPersonMPProjectile.cpp
@@ -0,0 +1,73 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ThirdPersonMPProjectile.h"
+#include "Components/SphereComponent.h"
+#include "Components/StaticMeshComponent.h"
+#include "GameFramework/ProjectileMovementComponent.h"
+#include "GameFramework/DamageType.h"
+#include "Particles/ParticleSystem.h"
+#include "Kismet/GameplayStatics.h"
+#include "UObject/ConstructorHelpers.h"
+
+// Sets default values
+AThirdPersonMPProjectile::AThirdPersonMPProjectile()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+	bReplicates = true;
+
+	//Definition for the SphereComponent that will serve as the Root component for the projectile and its collision.
+	SphereComponent = CreateDefaultSubobject<USphereComponent>(TEXT("RootComponent"));
+	SphereComponent->InitSphereRadius(37.5f);
+	SphereComponent->SetCollisionProfileName(TEXT("BlockAllDynamic"));
+	RootComponent = SphereComponent;
+
+	//Definition for the Mesh that will serve as our visual representation.
+	static ConstructorHelpers::FObjectFinder<UStaticMesh> DefaultMesh(TEXT("/Game/StarterContent/Shapes/Shape_Sphere.Shape_Sphere"));
+	StaticMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Mesh"));
+	StaticMesh->SetupAttachment(RootComponent);
+
+	//Set the Static Mesh and its position/scale if we successfully found a mesh asset to use.
+	if (DefaultMesh.Succeeded())
+	{
+		StaticMesh->SetStaticMesh(DefaultMesh.Object);
+		StaticMesh->RelativeLocation = FVector(0.0f, 0.0f, -37.5f);
+		StaticMesh->RelativeScale3D = FVector(0.75f, 0.75f, 0.75f);
+	}
+
+	// This will set the asset reference for our ExplosionEffect to be the P_Explosion asset inside of StarterContent.
+	static ConstructorHelpers::FObjectFinder<UParticleSystem> DefaultExplosionEffect(TEXT("/Game/StarterContent/Particles/P_Explosion.P_Explosion"));
+	if (DefaultExplosionEffect.Succeeded())
+	{
+		ExplosionEffect = DefaultExplosionEffect.Object;
+	}
+
+	//Definition for the Projectile Movement Component.
+	ProjectileMovementComponent = CreateDefaultSubobject<UProjectileMovementComponent>(TEXT("ProjectileMovement"));
+	ProjectileMovementComponent->SetUpdatedComponent(SphereComponent);
+	ProjectileMovementComponent->InitialSpeed = 1500.0f;
+	ProjectileMovementComponent->MaxSpeed = 1500.0f;
+	ProjectileMovementComponent->bRotationFollowsVelocity = true;
+	ProjectileMovementComponent->ProjectileGravityScale = 0.0f;
+
+	// These will initialize both the amount of Damage that the Projectile will deal to an Actor as well as the Damage Type that will be used in the damage event.
+	// Here we are initializing with the base UDamageType, as we have not yet defined any new Damage Types.
+	DamageType = UDamageType::StaticClass();
+	Damage = 10.0f;
+}
+
+// Called when the game starts or when spawned
+void AThirdPersonMPProjectile::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AThirdPersonMPProjectile::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
---
+++ Source/UE4_Multiplayer/Public/ThirdPersonMPProjectile.h
@@ -0,0 +1,45 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "ThirdPersonMPProjectile.generated.h"
+
+UCLASS()
+class UE4_MULTIPLAYER_API AThirdPersonMPProjectile : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AThirdPersonMPProjectile();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+public:
+	// Used to test collision
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Components")
+	class USphereComponent *SphereComponent;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Components")
+	class UStaticMeshComponent *StaticMesh;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Components")
+	class UProjectileMovementComponent *ProjectileMovementComponent;
+
+	UPROPERTY(EditAnywhere, Category="Effects")
+	class UParticleSystem *ExplosionEffect;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="Damage")
+	TSubclassOf<class UDamageType> DamageType;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="Damage")
+	float Damage;
+};
